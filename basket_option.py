53c723761acd975c5163e134762b8f163e921b6b
Tony Tang <u3528090@connect.hku.hk>
2021-04-07 19:35:35 +0800

    correct basket bug

--- basket_option.py
+++ basket_option.py
@@ -109,8 +109,6 @@ class basketArith(basketGeo):
             geoMean = np.exp((1/n) * (np.log(S_1) + np.log(S_2)))
             geoPayoff_call[i] = np.exp(-self.r*self.T)*max(geoMean-self.K, 0)
             geoPayoff_put[i] = np.exp(-self.r*self.T)*max(self.K-geoMean, 0)
-
-            # if i % 50 == 0:
                  
             if self.option_type == "call":
                 
